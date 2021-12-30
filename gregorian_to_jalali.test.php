3a1eba216ceac5a055a3dc21468427e3bfa364cd
Max Base <MaxBaseCode@Gmail.Com>
2021-12-30 09:52:22 +0330

    Create gregorian_to_jalali.test.php

---
+++ gregorian_to_jalali.test.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Language: PHP
+ * Date: 2021/12/30
+ * Name: gregorian_to_jalali
+ * Repository: https://github.com/BaseMax/gregorian_to_jalali
+ */
+
+include "gregorian_to_jalali.php";
+
+assert(gregorian_to_jalali_str(2021, 12, 30) === "1400/10/09");
