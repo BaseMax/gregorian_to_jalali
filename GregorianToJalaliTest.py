44aa669d359e3508f2401760f256945b6bf64efb
Max Base <MaxBaseCode@Gmail.Com>
2022-01-07 21:05:02 +0330

    Update GregorianToJalaliTest.py

--- GregorianToJalaliTest.py
+++ GregorianToJalaliTest.py
@@ -1,15 +1,14 @@
 import GregorianToJalali as GregorianToJalali
 from datetime import datetime
 
-# getting current date
+# Getting current date
 year = datetime.now().year
 month = datetime.now().month 
 day = datetime.now().day  
 
-
 g = GregorianToJalali.GregorianToJalali()
 
+# Tests
+print(g.gregorianToJalaliStr(2021,11,1))
 print(g.gregorianToJalaliStr(2022,1,7))
-
 print(g.gregorianToJalaliStr(year,month,day))
-
