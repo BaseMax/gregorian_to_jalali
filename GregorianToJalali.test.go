524238f919fbb75bc88a1b713df989cc31e89e77
Amin <aminasadiam@outlook.com>
2022-01-22 17:58:02 +0330

    change the date in unit testing

--- GregorianToJalali.test.go
+++ GregorianToJalali.test.go
@@ -6,9 +6,9 @@ import (
 )
 
 func TestGregorianToJalali(t *testing.T) {
-	result := GregorianToJalali(2022, 1, 21)
+	result := GregorianToJalali(2022, 1, 22)
 	date := fmt.Sprintf("%d/%d/%d", result.Year, result.Month, result.Day)
-	want := "1400/11/1"
+	want := "1400/11/02"
 	if date != want {
 		t.Errorf("got %s, want %s", date, want)
 	}
