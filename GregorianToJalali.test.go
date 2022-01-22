23065a83838d8a2bb5558ff122698979883aedc8
Max Base <MaxBaseCode@Gmail.Com>
2022-01-22 22:11:08 +0330

    add TODO to `GregorianToJalali.test.go`

--- GregorianToJalali.test.go
+++ GregorianToJalali.test.go
@@ -6,8 +6,10 @@ import (
 )
 
 func TestGregorianToJalali(t *testing.T) {
+	// TODO: We need to use a function or specially `assert` to easily can add more test cases!
+	// TODO: Please add more unit tests.
 	result := GregorianToJalali(2022, 1, 22)
-	date := fmt.Sprintf("%d/%d/%d", result.Year, result.Month, result.Day)
+	date := fmt.Sprintf("%d/%02d/%02d", result.Year, result.Month, result.Day)
 	want := "1400/11/02"
 	if date != want {
 		t.Errorf("got %s, want %s", date, want)
